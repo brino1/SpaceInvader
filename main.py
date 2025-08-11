fe3c40606f937c8103779e3ae2bb8303ae9f7104
Dev_bruno <brunooliveiracat35@gmail.com>
2025-08-11 19:13:51 -0300

    Finalizando o game

--- main.py
+++ main.py
@@ -50,10 +50,15 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 testY = 10
 
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
 def show_score(x, y):
     score = font.render("Score : "+ str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
 
+def game_over_text():
+    over_text = over_font.render("GAME OVER : " + str(score_value), True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -108,6 +113,12 @@ while running:
         playerX = 736
 
     for i in range(num_of_enemies):
+        # Game Over
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0:
             enemyX_change[i] = 4
