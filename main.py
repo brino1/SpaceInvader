4a704d89cb1438dcbf54143749d1f273da2b7495
Dev_bruno <brunooliveiracat35@gmail.com>
2025-08-11 15:53:58 -0300

    Adicionei mais imagens Setei um player

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('./asset/ufo.png')
 pygame.display.set_icon(icon)
+
+playerImg = pygame.image.load('./asset/player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 running = True
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
