f0fa13a8afb7f8f3f00746b52d744c5372be3d11
Dev_bruno <brunooliveiracat35@gmail.com>
2025-08-11 16:51:28 -0300

    Adicionei mecanicas de movimentos pelo teclado

--- main.py
+++ main.py
@@ -11,16 +11,29 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('./asset/player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 
 running = True
 while running:
     screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
-    player()
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
